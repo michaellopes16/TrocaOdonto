cee979870f23491a5156ffb21d3652ddc45b803f
Michael Lopes Bastos <mlb@dmz.org.br>
2022-01-29 00:19:13 -0300

    COD01: Add new View Pages and ViewModel componentes to use

--- TrocaOdonto/TrocaOdonto/TrocaOdonto/App.xaml.cs
+++ TrocaOdonto/TrocaOdonto/TrocaOdonto/App.xaml.cs
@@ -14,7 +14,8 @@ namespace TrocaOdonto
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new SplashPage());
         }
 
         protected override void OnStart()
