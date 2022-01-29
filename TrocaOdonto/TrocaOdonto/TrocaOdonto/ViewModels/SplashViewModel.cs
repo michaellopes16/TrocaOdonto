ae40efc5e5ff92c63d9a08e54de8807cd51ee9d2
Michael Lopes Bastos <mlb@dmz.org.br>
2022-01-29 01:13:28 -0300

    COD02: Add temporary splash  screen

--- TrocaOdonto/TrocaOdonto/TrocaOdonto/ViewModels/SplashViewModel.cs
+++ TrocaOdonto/TrocaOdonto/TrocaOdonto/ViewModels/SplashViewModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TrocaOdonto.ViewModels
 {
-    class SplashViewModel
+    public class SplashViewModel
     {
     }
 }
--- TrocaOdonto/TrocaOdonto/TrocaOdonto/Views/SplashPage.xaml.cs
+++ TrocaOdonto/TrocaOdonto/TrocaOdonto/Views/SplashPage.xaml.cs
@@ -12,9 +12,39 @@ namespace TrocaOdonto.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SplashPage : ContentPage
     {
+        Image splashImage;
+
         public SplashPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var sub = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "cart.png",
+                WidthRequest = 150,
+                HeightRequest = 150
+            };
+
+            AbsoluteLayout.SetLayoutFlags(splashImage,
+                AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage,
+                new Rectangle(0.5,0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            sub.Children.Add(splashImage);
+
+            this.BackgroundColor = Color.FromHex("#bce4ec");
+            this.Content = sub;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.9, 1500, Easing.Linear);
+            await splashImage.ScaleTo(150, 1200, Easing.Linear);
+            Application.Current.MainPage = new AppShell();
         }
     }
 }
