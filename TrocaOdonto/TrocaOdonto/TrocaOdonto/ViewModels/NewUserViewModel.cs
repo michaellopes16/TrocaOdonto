057bfa4b25c9a52e3d261f0cddeb293495e9409e
Michael Lopes Bastos <mlb@dmz.org.br>
2022-02-04 00:10:31 -0300

    COD04: Add fields in NewUserPage

--- TrocaOdonto/TrocaOdonto/TrocaOdonto/ViewModels/NewUserViewModel.cs
+++ TrocaOdonto/TrocaOdonto/TrocaOdonto/ViewModels/NewUserViewModel.cs
@@ -4,7 +4,62 @@ using System.Text;
 
 namespace TrocaOdonto.ViewModels
 {
-    class NewUserViewModel
+    public class NewUserViewModel:BaseViewModel
     {
+        private string _UserName;
+
+        public string UserName
+        {
+            get { return _UserName; }
+            set { _UserName = value; OnPropertyChanged(); }
+        }
+
+        private string _UserPassword;
+
+        public string UserPassword
+        {
+            get { return _UserPassword; }
+            set { _UserPassword = value; OnPropertyChanged(); }
+        }
+
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; OnPropertyChanged(); }
+        }
+
+        private string _DateOfBirth;
+
+        public string DateOfBirth
+        {
+            get { return _DateOfBirth; }
+            set { _DateOfBirth = value; OnPropertyChanged(); }
+        }
+
+        private string _IsFicalPeople;
+
+        public string IsFicalPeople
+        {
+            get { return _IsFicalPeople; }
+            set { _IsFicalPeople = value; OnPropertyChanged(); }
+        }
+
+        private string _UniqueIdentification;
+
+        public string UniqueIdentification
+        {
+            get { return _UniqueIdentification; }
+            set { _UniqueIdentification = value; OnPropertyChanged(); }
+        }
+
+        private string _CEP;
+
+        public string CEP
+        {
+            get { return _CEP; }
+            set { _CEP = value; OnPropertyChanged(); }
+        }
     }
 }
---
+++ TrocaOdonto/TrocaOdonto/TrocaOdonto/Views/UserControls/TrocaOdontoTextBoxEntry.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrocaOdonto.Views.UserControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrocaOdontoTextBoxEntry : ContentView
+    {
+
+        private string _FieldText;
+
+        public string FieldText
+        {
+            get { return _FieldText; }
+            set { _FieldText = value; }
+        }
+        private string _IconResource;
+
+        public string IconResource
+        {
+            get { return _IconResource; }
+            set { _IconResource = value; }
+        }
+
+        public TrocaOdontoTextBoxEntry()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
